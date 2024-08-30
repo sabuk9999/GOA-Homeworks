7cc819f24daf7318428419fbc9886ea8d4029c2f
unknown <Takidze.tsira@teachers.gov.ge>
2024-08-30 13:25:50 +0400

    commit

---
+++ level 16/homework.py
@@ -0,0 +1,68 @@
+#for i in range(10,20):
+#    print(i)
+
+#Task 1
+
+def greeting(num1,num2):
+    for i in range(num1,num2): 
+        print(i)
+greeting(10,20)
+
+
+
+#task 2
+
+name = input("Enter your name: ")
+print(f"My name is {name}")
+
+surname = input("Enter your surname: ")
+print(f"My surname is {surname}")
+
+academy = input("where you study in: ")
+print(f"i study in {academy}")
+
+
+#Task 3
+
+def greet_by_name(name):
+    print(f"hello {name}")
+greet_by_name("saba okradze")
+
+
+#task 4sa
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
